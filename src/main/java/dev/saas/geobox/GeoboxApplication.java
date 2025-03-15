49352f10e08ad57d19f4a29d9996de68534aa0ad
Eduardo <eduardodasilvadandolini@gmail.com>
2025-03-15 16:08:05 -0300

    Initial commit

---
+++ src/main/java/dev/saas/geobox/GeoboxApplication.java
@@ -0,0 +1,13 @@
+package dev.saas.geobox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoboxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/saas/geobox/GeoboxApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.saas.geobox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
